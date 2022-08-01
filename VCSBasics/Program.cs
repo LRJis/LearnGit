b61b962dcfbfc5f80c727a9baeb961efbc29a616
LRJis <1021291084@qq.com>
2022-08-01 18:07:41 +0800

    Add "Hello,git!"

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,6 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello,Git!");
             Console.WriteLine("Hello World!");
         }
     }
